625163efcfcafb838d405575b0bd37c4e65aeb9f
Igor Furgala <exploif@icloud.com>
2025-04-17 11:19:02 +0200

    fix: lint

--- src/ReactNativeRichTextEditorViewNativeComponent.ts
+++ src/ReactNativeRichTextEditorViewNativeComponent.ts
@@ -5,4 +5,6 @@ interface NativeProps extends ViewProps {
   color?: string;
 }
 
-export default codegenNativeComponent<NativeProps>('ReactNativeRichTextEditorView');
+export default codegenNativeComponent<NativeProps>(
+  'ReactNativeRichTextEditorView'
+);
