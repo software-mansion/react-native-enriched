48d013fbeb5fe932e8f3a82bc421a599fc00b24c
szydlovsky <9szydlowski9@gmail.com>
2025-05-26 14:38:21 +0200

    fix: pos-merge conflicts chaos

--- src/RichTextInput.tsx
+++ src/RichTextInput.tsx
@@ -69,8 +69,8 @@ export interface RichTextInputProps extends Omit<ViewProps, 'children'> {
   cursorColor?: ColorValue;
   selectionColor?: ColorValue;
   style?: ViewStyle | TextStyle;
-  onInputFocus?: () => void;
-  onInputBlur?: () => void;
+  onFocus?: () => void;
+  onBlur?: () => void;
   onChangeText?: (e: NativeSyntheticEvent<OnChangeTextEvent>) => void;
   onChangeHtml?: (e: NativeSyntheticEvent<OnChangeHtmlEvent>) => void;
   onChangeState?: (e: NativeSyntheticEvent<OnChangeStateEvent>) => void;
@@ -109,8 +109,8 @@ export const RichTextInput = ({
   cursorColor,
   selectionColor,
   style,
-  onInputFocus,
-  onInputBlur,
+  onFocus,
+  onBlur,
   onChangeText,
   onChangeHtml,
   onChangeState,
