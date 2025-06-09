fa9a7db080b79b81247697d10440347213d00b67
Igor Furgala <exploif@icloud.com>
2025-06-09 15:25:21 +0200

    feat: remove zero-width-space characters from the output

--- android/src/main/java/com/swmansion/reactnativerichtexteditor/spans/EditorH1Span.kt
+++ android/src/main/java/com/swmansion/reactnativerichtexteditor/spans/EditorH1Span.kt
@@ -3,8 +3,9 @@ package com.swmansion.reactnativerichtexteditor.spans
 import android.text.style.AbsoluteSizeSpan
 import com.swmansion.reactnativerichtexteditor.spans.interfaces.EditorHeadingSpan
 import com.swmansion.reactnativerichtexteditor.spans.interfaces.EditorInlineSpan
+import com.swmansion.reactnativerichtexteditor.spans.interfaces.EditorZeroWidthSpaceSpan
 import com.swmansion.reactnativerichtexteditor.styles.RichTextStyle
 
 // Heading spans inherit from EditorInlineSpan because they can be nested inside code block and block quote
-class EditorH1Span(style: RichTextStyle) : AbsoluteSizeSpan(style.h1FontSize), EditorHeadingSpan, EditorInlineSpan {
+class EditorH1Span(style: RichTextStyle) : AbsoluteSizeSpan(style.h1FontSize), EditorHeadingSpan, EditorInlineSpan, EditorZeroWidthSpaceSpan {
 }
