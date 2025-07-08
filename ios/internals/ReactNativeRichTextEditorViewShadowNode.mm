3b871c0ebc74c3bb2a861bfe6ecd6bf14642d3f1
Mikołaj Szydłowski <9szydlowski9@gmail.com>
2025-07-08 17:54:54 +0200

    iOS shadow node improvements (#103)
    * fix: real device main thread crash
    
    * fix: height computing delay heuristic

--- ios/internals/ReactNativeRichTextEditorViewShadowNode.mm
+++ ios/internals/ReactNativeRichTextEditorViewShadowNode.mm
@@ -3,6 +3,7 @@
 #import <react/utils/ManagedObjectWrapper.h>
 #import <yoga/Yoga.h>
 #import <React/RCTShadowView+Layout.h>
+#import "CoreText/CoreText.h"
 
 namespace facebook::react {
 
@@ -43,9 +44,37 @@ Size ReactNativeRichTextEditorViewShadowNode::measureContent(const LayoutContext
     ReactNativeRichTextEditorView *typedComponentObject = (ReactNativeRichTextEditorView *) componentObject;
     
     if(typedComponentObject != nullptr) {
-      CGSize estimatedSize = [typedComponentObject measureSize:layoutConstraints.maximumSize.width];
+      __block CGSize estimatedSize;
+      
+      // synchronously dispatch to main thread if needed
+      if([NSThread isMainThread]) {
+        estimatedSize = [typedComponentObject measureSize:layoutConstraints.maximumSize.width];
+      } else {
+        dispatch_sync(dispatch_get_main_queue(), ^{
+          estimatedSize = [typedComponentObject measureSize:layoutConstraints.maximumSize.width];
+        });
+      }
+      
       return {estimatedSize.width, estimatedSize.height};
     }
+  } else {
+    // on the very first call there is no componentView that we can query for the component height
+    // thus, a little heuristic: just put a height that is exactly height of letter "I" with default apple font and size from props
+    // in a lot of cases it will be the desired height
+    // in others, the jump on the second call will at least be smaller
+    const auto props = this->getProps();
+    const auto &typedProps = *std::static_pointer_cast<ReactNativeRichTextEditorViewProps const>(props);
+    NSAttributedString *attrStr = [[NSAttributedString alloc] initWithString:@"I" attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:typedProps.fontSize]}];
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)attrStr);
+    const CGSize &suggestedSize = CTFramesetterSuggestFrameSizeWithConstraints(
+      framesetter,
+      CFRangeMake(0, 1),
+      nullptr,
+      CGSizeMake(layoutConstraints.maximumSize.width, DBL_MAX),
+      nullptr
+    );
+    
+    return {suggestedSize.width, suggestedSize.height};
   }
   
   return Size();
