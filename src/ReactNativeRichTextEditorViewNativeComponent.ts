490f1ae041c95e7b19c40f9f039d8b78b182f1b6
szydlovsky <9szydlowski9@gmail.com>
2025-07-14 16:14:27 +0200

    fix: update workaround comment

--- src/ReactNativeRichTextEditorViewNativeComponent.ts
+++ src/ReactNativeRichTextEditorViewNativeComponent.ts
@@ -98,8 +98,7 @@ export interface RichTextStyleInternal {
     textDecorationLine?: string;
   };
   // This is a workaround for the fact that codegen does not support Records.
-  // Instead, we pass an empty object as a type.
-  // However, this object is not empty in practice, and will become ReadableMap on the native side
+  // On native Android side this will become a ReadableMap, on nativeiOS we can work with a folly::dynamic object.
   mention?: UnsafeMixed;
   img?: {
     width?: Float;
