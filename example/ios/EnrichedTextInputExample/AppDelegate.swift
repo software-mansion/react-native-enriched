f5888b4d2c8b072109cd659ab10937919d5279ad
szydlovsky <9szydlowski9@gmail.com>
2025-08-29 17:45:56 +0200

    chore: apply all the renames in the example ios app

--- example/ios/EnrichedTextInputExample/AppDelegate.swift
+++ example/ios/EnrichedTextInputExample/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "ReactNativeRichTextEditorExample",
+      withModuleName: "EnrichedTextInputExample",
       in: window,
       launchOptions: launchOptions
     )
