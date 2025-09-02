25815757203d7843aaa88081185384dc8cebad10
Igor Furgała <74370735+exploIF@users.noreply.github.com>
2025-09-02 17:19:06 +0200

    chore: improve example app styling (#136)

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -219,7 +219,7 @@ export default function App() {
         style={styles.container}
         contentContainerStyle={styles.content}
       >
-        <Text style={styles.label}>SWM Rich Text Editor</Text>
+        <Text style={styles.label}>react-native-enriched</Text>
         <View style={styles.editor}>
           <EnrichedTextInput
             autoFocus
@@ -228,9 +228,9 @@ export default function App() {
             style={styles.editorInput}
             htmlStyle={htmlStyle}
             placeholder="Type something here..."
-            placeholderTextColor="blue"
-            selectionColor="red"
-            cursorColor="yellow"
+            placeholderTextColor="rgb(0, 26, 114)"
+            selectionColor="deepskyblue"
+            cursorColor="dodgerblue"
             autocapitalize="sentences"
             defaultValue={defaultValue}
             onChangeText={handleChangeText}
@@ -371,10 +371,10 @@ const styles = StyleSheet.create({
   editorInput: {
     marginTop: 24,
     width: '100%',
-    maxHeight: 120,
+    maxHeight: 240,
     backgroundColor: 'gainsboro',
     fontSize: 18,
-    fontFamily: 'CascadiaCode-Regular',
+    fontFamily: 'Nunito-Regular',
     paddingVertical: 12,
     paddingHorizontal: 14,
   },
