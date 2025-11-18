4467366d170472323f6f17a9103c75dc1078bfee
Igor Furgała <74370735+exploIF@users.noreply.github.com>
2025-11-18 10:35:33 +0100

    chore: revert iOS parser changes

--- ios/inputParser/InputParser.mm
+++ ios/inputParser/InputParser.mm
@@ -597,12 +597,10 @@
     } else if([tagName isEqualToString:@"code"]) {
       [styleArr addObject:@([InlineCodeStyle getStyleType])];
     } else if([tagName isEqualToString:@"a"]) {
-       [styleArr addObject:@([LinkStyle getStyleType])];
-       NSRange hrefRange = [params rangeOfString:@"href="];
-       if(hrefRange.location != NSNotFound) {
-         NSString *url = [params substringWithRange:NSMakeRange(6, params.length - 7)];
-         stylePair.styleValue = url;
-       }
+      [styleArr addObject:@([LinkStyle getStyleType])];
+      // cut only the url from the href="..." string
+      NSString *url = [params substringWithRange:NSMakeRange(6, params.length - 7)];
+      stylePair.styleValue = url;
     } else if([tagName isEqualToString:@"mention"]) {
       [styleArr addObject:@([MentionStyle getStyleType])];
       // extract html expression into dict using some regex
