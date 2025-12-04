6aad08e0c585f8fcd88d1d7d19b51a2720f933d2
Kacper Żółkiewski <74975508+kacperzolkiewski@users.noreply.github.com>
2025-12-04 13:58:21 +0100

    feat: add support for remote images on Android (#301)
    # Summary
    
    This PR:
    
    -  adds support for remote images on Android 
    - use local asset
    [broken_image](https://fonts.google.com/icons?selected=Material+Symbols+Outlined:broken_image:FILL@0;wght@400;GRAD@0;opsz@24&icon.query=broken+ima&icon.size=24&icon.color=%23e3e3e3)
    as a fallback on Android
    -  extends example to handle aspect ratio when adding images
    
    ## Test Plan
    Run Example app and try to add remote images:
    
    1. through toolbar menu
    2. through `Set input's value` button (provide html with remote img tag)
    
    ## Screenshots / Videos
    
    
    
    
    https://github.com/user-attachments/assets/9a623128-bac1-4763-b2e0-9179cbc1c473
    
    
    
    
    https://github.com/user-attachments/assets/6c4f1679-a67f-4beb-b8c8-824fe05a5797
    
    
    
    
    
    ## Compatibility
    
    | OS      | Implemented |
    | ------- | :---------: |
    | iOS     |    ❌     |
    | Android |    ✅     |
    
    ---------

--- android/src/main/java/com/swmansion/enriched/EnrichedTextInputViewPackage.kt
+++ android/src/main/java/com/swmansion/enriched/EnrichedTextInputViewPackage.kt
@@ -4,10 +4,12 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
+import com.swmansion.enriched.utils.ResourceManager
 import java.util.ArrayList
 
 class EnrichedTextInputViewPackage : ReactPackage {
   override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    ResourceManager.init(reactContext.applicationContext)
     val viewManagers: MutableList<ViewManager<*, *>> = ArrayList()
     viewManagers.add(EnrichedTextInputViewManager())
     return viewManagers
--- android/src/main/java/com/swmansion/enriched/utils/EnrichedParser.java
+++ android/src/main/java/com/swmansion/enriched/utils/EnrichedParser.java
@@ -1,9 +1,5 @@
 package com.swmansion.enriched.utils;
 
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.text.Editable;
 import android.text.Layout;
@@ -13,7 +9,6 @@ import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.style.AlignmentSpan;
 import android.text.style.ParagraphStyle;
-import android.util.Log;
 
 import com.swmansion.enriched.spans.EnrichedBlockQuoteSpan;
 import com.swmansion.enriched.spans.EnrichedBoldSpan;
@@ -697,46 +692,10 @@ class HtmlToSpannedConverter implements ContentHandler {
     String width = attributes.getValue("", "width");
     String height = attributes.getValue("", "height");
 
-    Drawable d = null;
-    if (img != null) {
-      d = img.getDrawable(src);
-    }
-
-    if (d == null) {
-      d = HtmlToSpannedConverter.prepareDrawableForImage(src);
-    }
-
-    if (d == null) {
-      return;
-    }
-
     int len = text.length();
+    EnrichedImageSpan span = EnrichedImageSpan.Companion.createEnrichedImageSpan(src, Integer.parseInt(width), Integer.parseInt(height));
     text.append("￼");
-    text.setSpan(new EnrichedImageSpan(d, src, Integer.parseInt(width), Integer.parseInt(height)), len, text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-  }
-
-  private static BitmapDrawable prepareDrawableForImage(String src) {
-    String cleanPath = src;
-    if (cleanPath.startsWith("file://")) {
-      cleanPath = cleanPath.substring(7);
-    }
-
-    BitmapDrawable drawable = null;
-
-    try {
-      Bitmap bitmap = BitmapFactory.decodeFile(cleanPath);
-      if (bitmap != null) {
-        drawable = new BitmapDrawable(Resources.getSystem(), bitmap);
-        // set bounds so it knows how big it is naturally,
-        // though EnrichedImageSpan will override this with the HTML width/height later.
-        drawable.setBounds(0, 0, bitmap.getWidth(), bitmap.getHeight());
-      }
-    } catch (Exception e) {
-      // Failed to load file
-      Log.e("EnrichedParser", "Failed to load image from path: " + cleanPath, e);
-    }
-
-    return drawable;
+    text.setSpan(span, len, text.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
   }
 
   private static void startA(Editable text, Attributes attributes) {
