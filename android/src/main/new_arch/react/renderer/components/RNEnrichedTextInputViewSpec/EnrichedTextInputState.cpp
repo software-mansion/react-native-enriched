a7111253dfca6b4156a304e80191ec01dde2e306
Igor Furgala <exploif@icloud.com>
2025-12-10 12:59:23 +0100

    chore: fix and ready

--- android/src/main/new_arch/react/renderer/components/RNEnrichedTextInputViewSpec/EnrichedTextInputState.cpp
+++ android/src/main/new_arch/react/renderer/components/RNEnrichedTextInputViewSpec/EnrichedTextInputState.cpp
@@ -3,7 +3,7 @@
 namespace facebook::react {
 
 int EnrichedTextInputState::getForceHeightRecalculationCounter() const {
-  return forceHeightRecalculationCounter_ ;
+  return forceHeightRecalculationCounter_;
 }
 
 } // namespace facebook::react
