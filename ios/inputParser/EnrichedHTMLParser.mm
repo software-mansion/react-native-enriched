c565e2ff3afad9be7868ff1c30c49aca55ad98bd
IvanIhnatsiuk <ivan.ihnatsiuk@bankifi.com>
2025-12-14 22:47:59 +0100

    feat: faster html generation

--- ios/inputParser/EnrichedHTMLParser.mm
+++ ios/inputParser/EnrichedHTMLParser.mm
@@ -5,8 +5,8 @@
 
 @implementation EnrichedHTMLParser {
   NSDictionary<NSNumber *, id<BaseStyleProtocol>> *_styles;
-  NSArray<NSNumber *> *_inlineOrder;
-  NSArray<NSNumber *> *_paragraphOrder;
+  NSArray<id<BaseStyleProtocol>> *_inlineStyles;
+  NSArray<id<BaseStyleProtocol>> *_paragraphStyles;
 }
 
 - (instancetype)initWithStyles:(NSDictionary<NSNumber *, id> *)stylesDict {
@@ -16,27 +16,27 @@
 
   _styles = stylesDict ?: @{};
 
-  NSMutableArray *inlineArr = [NSMutableArray array];
-  NSMutableArray *paragraphArr = [NSMutableArray array];
+  NSMutableArray *inlineStylesArray = [NSMutableArray array];
+  NSMutableArray *paragraphStylesArray = [NSMutableArray array];
 
-  for (NSNumber *type in _styles) {
-    id<BaseStyleProtocol> style = _styles[type];
+  NSArray *allKeys = stylesDict.allKeys;
+  for (NSInteger i = 0; i < allKeys.count; i++) {
+    NSNumber *key = allKeys[i];
+    id<BaseStyleProtocol> style = stylesDict[key];
     Class cls = style.class;
 
-    BOOL isParagraph = ([cls respondsToSelector:@selector(isParagraphStyle)] &&
-                        [cls isParagraphStyle]);
+    BOOL isParagraph = ([cls respondsToSelector:@selector(isParagraphStyle)]) &&
+                       [cls isParagraphStyle];
 
-    if (isParagraph)
-      [paragraphArr addObject:type];
-    else
-      [inlineArr addObject:type];
+    if (isParagraph) {
+      [paragraphStylesArray addObject:style];
+    } else {
+      [inlineStylesArray addObject:style];
+    }
   }
 
-  [inlineArr sortUsingSelector:@selector(compare:)];
-  [paragraphArr sortUsingSelector:@selector(compare:)];
-
-  _inlineOrder = inlineArr.copy;
-  _paragraphOrder = paragraphArr.copy;
+  _inlineStyles = inlineStylesArray.copy;
+  _paragraphStyles = paragraphStylesArray.copy;
 
   return self;
 }
@@ -49,10 +49,10 @@
 
   HTMLElement *root = [self buildRootNodeFromAttributedString:text];
 
-  NSMutableData *buf = [NSMutableData data];
-  [self createHtmlFromNode:root into:buf pretify:pretify];
+  NSMutableData *buffer = [NSMutableData data];
+  [self createHtmlFromNode:root into:buffer pretify:pretify];
 
-  return [[NSString alloc] initWithData:buf encoding:NSUTF8StringEncoding];
+  return [[NSString alloc] initWithData:buffer encoding:NSUTF8StringEncoding];
 }
 
 - (HTMLElement *)buildRootNodeFromAttributedString:(NSAttributedString *)text {
@@ -65,7 +65,7 @@
   br.tag = "br";
   br.selfClosing = YES;
 
-  __block NSNumber *previousParagraphType = nil;
+  __block id<BaseStyleProtocol> previousParagraphStyle = nil;
   __block HTMLElement *previousNode = nil;
 
   [plain
@@ -77,37 +77,27 @@
                                    BOOL *__unused stop) {
                         if (paragraphRange.length == 0) {
                           [root.children addObject:br];
-                          previousParagraphType = nil;
+                          previousParagraphStyle = nil;
                           previousNode = nil;
                           return;
                         }
-                        NSDictionary *attrsAtStart =
-                            [text attributesAtIndex:paragraphRange.location
-                                     effectiveRange:nil];
 
-                        NSNumber *ptype = nil;
-                        for (NSNumber *sty in self->_paragraphOrder) {
-                          id<BaseStyleProtocol> s = self->_styles[sty];
-                          NSString *key = [s.class attributeKey];
-                          id val = attrsAtStart[key];
-                          if (val && [s styleCondition:val
-                                                 range:paragraphRange]) {
-                            ptype = sty;
-                            break;
-                          }
-                        }
+                        id<BaseStyleProtocol> paragraphStyle =
+                            [self detectParagraphStyle:text
+                                        paragraphRange:paragraphRange];
 
-                        HTMLElement *container =
-                            [self containerForBlock:ptype
-                                        reuseLastOf:previousParagraphType
-                                       previousNode:previousNode
-                                           rootNode:root];
+                        HTMLElement *container = [self
+                            containerForParagraphStyle:paragraphStyle
+                                previousParagraphStyle:previousParagraphStyle
+                                          previousNode:previousNode
+                                              rootNode:root];
 
-                        previousParagraphType = ptype;
+                        previousParagraphStyle = paragraphStyle;
                         previousNode = container;
 
-                        HTMLElement *target = [self getNextContainer:ptype
-                                                    currentContainer:container];
+                        HTMLElement *target =
+                            [self nextContainerForParagraphStyle:paragraphStyle
+                                                currentContainer:container];
 
                         [text
                             enumerateAttributesInRange:paragraphRange
@@ -128,10 +118,47 @@
   return root;
 }
 
-- (HTMLElement *)containerForBlock:(NSNumber *)currentParagraphType
-                       reuseLastOf:(NSNumber *)previousParagraphType
-                      previousNode:(HTMLElement *)previousNode
-                          rootNode:(HTMLElement *)rootNode {
+- (HTMLElement *)nextContainerForParagraphStyle:
+                     (id<BaseStyleProtocol> _Nullable)style
+                               currentContainer:(HTMLElement *)container {
+  if (!style)
+    return container;
+
+  const char *sub = [style.class subTagName];
+  if (!sub)
+    return container;
+
+  HTMLElement *inner = [HTMLElement new];
+  inner.tag = sub;
+  [container.children addObject:inner];
+  return inner;
+}
+
+- (id<BaseStyleProtocol> _Nullable)
+    detectParagraphStyle:(NSAttributedString *)text
+          paragraphRange:(NSRange)paragraphRange {
+  NSDictionary *attrsAtStart = [text attributesAtIndex:paragraphRange.location
+                                        effectiveRange:nil];
+  id<BaseStyleProtocol> _Nullable foundParagraphStyle = nil;
+  for (NSInteger i = 0; i < _paragraphStyles.count; i++) {
+    id<BaseStyleProtocol> paragraphStyle = _paragraphStyles[i];
+    Class paragraphStyleClass = paragraphStyle.class;
+
+    NSAttributedStringKey attributeKey = [paragraphStyleClass attributeKey];
+    id value = attrsAtStart[attributeKey];
+
+    if (value && [paragraphStyle styleCondition:value range:paragraphRange]) {
+      return paragraphStyle;
+    }
+  }
+
+  return foundParagraphStyle;
+}
+
+- (HTMLElement *)currentParagraphType:(NSNumber *)currentParagraphType
+                previousParagraphType:(NSNumber *)previousParagraphType
+                         previousNode:(HTMLElement *)previousNode
+                             rootNode:(HTMLElement *)rootNode {
   if (!currentParagraphType) {
     HTMLElement *outer = [HTMLElement new];
     outer.tag = "p";
@@ -156,25 +183,31 @@
   return outer;
 }
 
-- (HTMLElement *)getNextContainer:(NSNumber *)blockType
-                 currentContainer:(HTMLElement *)container {
-
-  if (!blockType)
-    return container;
-
-  id<BaseStyleProtocol> style = _styles[blockType];
-
-  const char *subTagName = [style.class subTagName];
-
-  if (subTagName) {
-    HTMLElement *inner = [HTMLElement new];
-    inner.tag = subTagName;
-    [container.children addObject:inner];
-    return inner;
+- (HTMLElement *)
+    containerForParagraphStyle:(id<BaseStyleProtocol> _Nullable)currentStyle
+        previousParagraphStyle:(id<BaseStyleProtocol> _Nullable)previousStyle
+                  previousNode:(HTMLElement *)previousNode
+                      rootNode:(HTMLElement *)rootNode {
+  if (!currentStyle) {
+    HTMLElement *outer = [HTMLElement new];
+    outer.tag = "p";
+    [rootNode.children addObject:outer];
+    return outer;
   }
 
-  return container;
+  BOOL sameStyle = (currentStyle == previousStyle);
+  Class styleClass = currentStyle.class;
+  BOOL hasSub = ([styleClass subTagName] != NULL);
+
+  if (sameStyle && hasSub)
+    return previousNode;
+
+  HTMLElement *outer = [HTMLElement new];
+  outer.tag = [styleClass tagName];
+  [rootNode.children addObject:outer];
+  return outer;
 }
+
 - (HTMLNode *)getInlineStyleNodes:(NSAttributedString *)text
                             range:(NSRange)range
                             attrs:(NSDictionary *)attrs
@@ -184,13 +217,12 @@
   textNode.range = range;
   HTMLNode *currentNode = textNode;
 
-  for (NSNumber *inlineStyleType in _inlineOrder) {
-
-    id<BaseStyleProtocol> styleObject = _styles[inlineStyleType];
+  for (NSInteger i = 0; i < _inlineStyles.count; i++) {
+    id<BaseStyleProtocol> styleObject = _inlineStyles[i];
     Class styleClass = styleObject.class;
 
-    NSString *key = [styleClass attributeKey];
-    id value = attrs[key];
+    NSAttributedStringKey attributeKey = [styleClass attributeKey];
+    id value = attrs[attributeKey];
 
     if (!value || ![styleObject styleCondition:value range:range])
       continue;
