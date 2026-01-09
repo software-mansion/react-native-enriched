ebe6cbe285db9e29062be9cc0bf279e9988ef1fd
szydlovsky <9szydlowski9@gmail.com>
2026-01-09 16:14:31 +0100

    fix: remove empty line

--- ios/config/InputConfig.mm
+++ ios/config/InputConfig.mm
@@ -487,7 +487,6 @@
   _linkRegexConfig = newValue;
 
   // try initializing the native regular expression if it applies
-
   if (_linkRegexConfig.isDefault || _linkRegexConfig.isDisabled) {
     return;
   }
