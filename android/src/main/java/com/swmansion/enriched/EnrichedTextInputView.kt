software-mansion/react-native-enriched

commit 728cd2f81529877482b7bfb3fc366f7695924bab

fix: selecting text - recycled android view (#343)
# Summary

This PR should fix issues with selecting text on Android when view is
recycled. Inspired by [react-native
logic](https://github.com/facebook/react-native/blob/36df97f500aa0aa8031098caf7526db358b6ddc1/packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.kt#L946)

## Test Plan

1. Enter below code in the example app:

```tsx
      <FlatList
        style={styles.container}
        data={[1, 2, 1]}
        renderItem={({ item }) => {
          if (item == 1) {
            return <View style={styles.scrollPlaceholder} />;
          }
          return (
            <View style={{ flex: 1 }}>
              <View style={styles.editor}>
                <EnrichedTextInput
                  ref={ref}
                  mentionIndicators={['@', '#']}
                  style={styles.editorInput}
                  htmlStyle={htmlStyle}
                  placeholder="Type something here..."
                  placeholderTextColor="rgb(0, 26, 114)"
                  selectionColor="deepskyblue"
                  cursorColor="dodgerblue"
                  autoCapitalize="sentences"
                  onChangeText={(e) => handleChangeText(e.nativeEvent)}
                  onChangeHtml={(e) => handleChangeHtml(e.nativeEvent)}
                  onChangeState={(e) => handleChangeState(e.nativeEvent)}
                  onLinkDetected={handleLinkDetected}
                  onMentionDetected={console.log}
                  onStartMention={handleStartMention}
                  onChangeMention={handleChangeMention}
                  onEndMention={handleEndMention}
                  onFocus={handleFocusEvent}
                  onBlur={handleBlurEvent}
                  onChangeSelection={(e) =>
                    handleSelectionChangeEvent(e.nativeEvent)
                  }
                  androidExperimentalSynchronousEvents={

android/src/main/java/com/swmansion/enriched/EnrichedTextInputView.kt
@@ -735,6 +735,9 @@ class EnrichedTextInputView : AppCompatEditText {
   override fun onAttachedToWindow() {
     super.onAttachedToWindow()
 
+    // https://github.com/facebook/react-native/blob/36df97f500aa0aa8031098caf7526db358b6ddc1/packages/react-native/ReactAndroid/src/main/java/com/facebook/react/views/textinput/ReactEditText.kt#L946
+    super.setTextIsSelectable(true)
+
     if (autoFocus && !didAttachToWindow) {
       requestFocusProgrammatically()
     }

commit 7b6ad50a591661314283b1b49713cb9609b9986e

fix: android span watcher (#359)
# Summary

Issue has been originally introduced in
https://github.com/software-mansion/react-native-enriched/pull/343, but
in general it never worked properly. Span watcher has to be attached for
the whole view lifecycle, it used for handling some styling related
logic. Previously we were applying it manually, but when editable
instance changed it wasn't re-created. With current approach we use
editable factory to ensure it's attached once new editable instance is
created.

## Test Plan

- created ordered list with at least two elements
- remove first element
- notice that second element index changed (while it won't change on
current main branch)

## Screenshots / Videos


https://github.com/user-attachments/assets/942e8a71-a538-4e54-8269-bb446bf3b96e


## Compatibility

| OS      | Implemented |
| ------- | :---------: |
| iOS     |    ❌     |
| Android |    ✅     |

android/src/main/java/com/swmansion/enriched/EnrichedTextInputView.kt
@@ -41,6 +41,7 @@ import com.swmansion.enriched.styles.InlineStyles
 import com.swmansion.enriched.styles.ListStyles
 import com.swmansion.enriched.styles.ParagraphStyles
 import com.swmansion.enriched.styles.ParametrizedStyles
+import com.swmansion.enriched.utils.EnrichedEditableFactory
 import com.swmansion.enriched.utils.EnrichedParser
 import com.swmansion.enriched.utils.EnrichedSelection
 import com.swmansion.enriched.utils.EnrichedSpanState
@@ -124,7 +125,11 @@ class EnrichedTextInputView : AppCompatEditText {
     setPadding(0, 0, 0, 0)
     setBackgroundColor(Color.TRANSPARENT)
 
-    addSpanWatcher(EnrichedSpanWatcher(this))
+    // Ensure that every time new editable is created, it has EnrichedSpanWatcher attached
+    val spanWatcher = EnrichedSpanWatcher(this)
+    this.spanWatcher = spanWatcher
+    setEditableFactory(EnrichedEditableFactory(spanWatcher))
+
     addTextChangedListener(EnrichedTextWatcher(this))
   }
 
@@ -275,8 +280,6 @@ class EnrichedTextInputView : AppCompatEditText {
       setText(newText)
 
       observeAsyncImages()
-      // Assign SpanWatcher one more time as our previous spannable has been replaced
-      addSpanWatcher(EnrichedSpanWatcher(this))
 
       // Scroll to the last line of text
       setSelection(text?.length ?: 0)
@@ -588,12 +591,6 @@ class EnrichedTextInputView : AppCompatEditText {
     }
 
     return true
-  }
-
-  private fun addSpanWatcher(watcher: EnrichedSpanWatcher) {
-    val spannable = text as Spannable
-    spannable.setSpan(watcher, 0, spannable.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
-    spanWatcher = watcher
   }
 
   fun verifyAndToggleStyle(name: String) {

android/src/main/java/com/swmansion/enriched/utils/EnrichedEditableFactory.kt
@@ -1,0 +1,17 @@
+package com.swmansion.enriched.utils
+
+import android.text.Editable
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import com.swmansion.enriched.watchers.EnrichedSpanWatcher
+
+class EnrichedEditableFactory(
+  private val watcher: EnrichedSpanWatcher,
+) : Editable.Factory() {
+  override fun newEditable(source: CharSequence): Editable {
+    val s = source as? SpannableStringBuilder ?: SpannableStringBuilder(source)
+    s.removeSpan(watcher)
+    s.setSpan(watcher, 0, s.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+    return s
+  }
+}