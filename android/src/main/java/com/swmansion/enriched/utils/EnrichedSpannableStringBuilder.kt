7fc69b1a79876263f8b71560cc4eb97f300e704d
Igor Furgala <exploif@icloud.com>
2026-01-14 12:08:27 +0100

    chore: cleanup extensions

---
+++ android/src/main/java/com/swmansion/enriched/utils/EnrichedSpannableStringBuilder.kt
@@ -0,0 +1,15 @@
+package com.swmansion.enriched.utils
+
+import android.text.SpannableStringBuilder
+
+// Removes zero-width spaces from the given range in the SpannableStringBuilder without affecting spans
+fun SpannableStringBuilder.removeZWS(
+  start: Int,
+  end: Int,
+) {
+  for (i in (end - 1) downTo start) {
+    if (this[i] == '\u200B') {
+      delete(i, i + 1)
+    }
+  }
+}
