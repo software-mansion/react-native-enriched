def666a770816cb2b102fdb6270854af84efaad9
Igor Furgala <exploif@icloud.com>
2026-01-15 11:09:57 +0100

    fix: building RN 0.79 and RN 0.80

--- android/src/main/new_arch/react/renderer/components/RNEnrichedTextInputViewSpec/conversions.h
+++ android/src/main/new_arch/react/renderer/components/RNEnrichedTextInputViewSpec/conversions.h
@@ -1,9 +1,14 @@
 #pragma once
 
 #include <folly/dynamic.h>
-#include <react/renderer/components/FBReactNativeSpec/Props.h>
 #include <react/renderer/components/RNEnrichedTextInputViewSpec/Props.h>
 #include <react/renderer/core/propsConversions.h>
+
+#if REACT_NATIVE_MINOR_VERSION >= 81
+#include <react/renderer/components/FBReactNativeSpec/Props.h>
+#else
+#include <react/renderer/components/rncore/Props.h>
+#endif
 
 namespace facebook::react {
 
@@ -18,6 +23,21 @@ inline folly::dynamic toDynamic(const EnrichedTextInputViewProps &props) {
   serializedProps["fontStyle"] = props.fontStyle;
   serializedProps["fontFamily"] = props.fontFamily;
   serializedProps["htmlStyle"] = toDynamic(props.htmlStyle);
+
+  return serializedProps;
+}
+#elif REACT_NATIVE_MINOR_VERSION >= 79
+inline folly::dynamic toDynamic(const EnrichedTextInputViewProps &props) {
+  folly::dynamic serializedProps = folly::dynamic::object();
+  serializedProps["defaultValue"] = props.defaultValue;
+  serializedProps["placeholder"] = props.placeholder;
+  serializedProps["fontSize"] = props.fontSize;
+  serializedProps["fontWeight"] = props.fontWeight;
+  serializedProps["fontStyle"] = props.fontStyle;
+  serializedProps["fontFamily"] = props.fontFamily;
+  // Ideally we should also serialize htmlStyle, but toDynamic function is not
+  // generated in this RN version As RN 0.79 and 0.80 is no longet supported, we
+  // can skip it for now
 
   return serializedProps;
 }
