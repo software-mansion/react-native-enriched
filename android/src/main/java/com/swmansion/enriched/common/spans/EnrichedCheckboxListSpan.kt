7ae7f49308992c22ae621f2b684b8081158339f9
Igor Furgała <74370735+exploIF@users.noreply.github.com>
2026-01-26 15:45:40 +0100

    chore: move CheckboxDrawable to common (#395)
    # Summary
    
    Moves `CheckboxDrawable` to common
    
    ## Test Plan
    
    Check if it builds fine
    
    ## Screenshots / Videos
    
    n/a
    
    ## Compatibility
    
    | OS      | Implemented |
    | ------- | :---------: |
    | iOS     |    ❌     |
    | Android |    ✅     |

--- android/src/main/java/com/swmansion/enriched/common/spans/EnrichedCheckboxListSpan.kt
+++ android/src/main/java/com/swmansion/enriched/common/spans/EnrichedCheckboxListSpan.kt
@@ -9,10 +9,10 @@ import android.text.style.LeadingMarginSpan
 import android.text.style.LineHeightSpan
 import android.text.style.MetricAffectingSpan
 import androidx.core.graphics.withTranslation
+import com.swmansion.enriched.common.CheckboxDrawable
 import com.swmansion.enriched.common.EnrichedStyle
 import com.swmansion.enriched.common.spans.interfaces.EnrichedParagraphSpan
 import com.swmansion.enriched.textinput.styles.HtmlStyle
-import com.swmansion.enriched.utils.CheckboxDrawable
 
 open class EnrichedCheckboxListSpan(
   open var isChecked: Boolean,
