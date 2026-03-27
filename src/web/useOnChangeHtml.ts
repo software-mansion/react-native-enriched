017ec02726ccc0cf452e313d24cccaf75de37555
Piotr Karamon <piotrkaramon3@gmail.com>
2026-03-27 16:42:37 +0100

    refactor: onChangeHtml/Text

--- src/web/useOnChangeHtml.ts
+++ src/web/useOnChangeHtml.ts
@@ -1,35 +1,12 @@
-import { useEffect, useRef } from 'react';
 import { type Editor } from '@tiptap/react';
 import type { OnChangeHtmlEvent } from '../types';
 import type { NativeSyntheticEvent } from 'react-native';
-import { adaptWebToNativeEvent } from './adaptWebToNativeEvent';
+import { useOnEditorChange } from './useOnEditorChange';
 import getNormalizedHtml from './getNormalizedHtml';
 
 export const useOnChangeHtml = (
   editor: Editor,
   onChangeHtml?: (e: NativeSyntheticEvent<OnChangeHtmlEvent>) => void
 ) => {
-  const lastHtmlRef = useRef('');
-
-  useEffect(() => {
-    if (!onChangeHtml) return;
-
-    const handleUpdate = () => {
-      const html = getNormalizedHtml(editor);
-
-      if (html !== lastHtmlRef.current) {
-        lastHtmlRef.current = html;
-        onChangeHtml(adaptWebToNativeEvent(null, { value: html }));
-      }
-    };
-
-    handleUpdate();
-
-    editor.on('transaction', handleUpdate);
-    editor.on('transaction', () => {});
-
-    return () => {
-      editor.off('transaction', handleUpdate);
-    };
-  }, [editor, onChangeHtml]);
+  useOnEditorChange(editor, onChangeHtml, getNormalizedHtml);
 };
--- src/web/useOnChangeText.ts
+++ src/web/useOnChangeText.ts
@@ -1,33 +1,13 @@
-import { useEffect, useRef } from 'react';
 import { type Editor } from '@tiptap/react';
 import type { OnChangeTextEvent } from '../types';
 import type { NativeSyntheticEvent } from 'react-native';
-import { adaptWebToNativeEvent } from './adaptWebToNativeEvent';
+import { useOnEditorChange } from './useOnEditorChange';
 
 export const useOnChangeText = (
   editor: Editor,
   onChangeText?: (e: NativeSyntheticEvent<OnChangeTextEvent>) => void
 ) => {
-  const lastTextRef = useRef('');
-
-  useEffect(() => {
-    if (!onChangeText) return;
-
-    const handleUpdate = () => {
-      const text = editor.getText({ blockSeparator: '\n' });
-
-      if (text !== lastTextRef.current) {
-        lastTextRef.current = text;
-        onChangeText(adaptWebToNativeEvent(null, { value: text }));
-      }
-    };
-
-    handleUpdate();
-
-    editor.on('transaction', handleUpdate);
-
-    return () => {
-      editor.off('transaction', handleUpdate);
-    };
-  }, [editor, onChangeText]);
+  useOnEditorChange(editor, onChangeText, (e) =>
+    e.getText({ blockSeparator: '\n' })
+  );
 };
