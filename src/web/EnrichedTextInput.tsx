7f354020e8493ba7fdd679921fcc8e74b9fb67e9
Krystian Sienkiewicz <krystian.sienkiewicz@swmansion.com>
2026-05-29 16:00:41 +0200

    feat(web): undo, redo

--- src/web/EnrichedTextInput.tsx
+++ src/web/EnrichedTextInput.tsx
@@ -26,6 +26,7 @@ import {
 import Document from '@tiptap/extension-document';
 import Paragraph from '@tiptap/extension-paragraph';
 import Text from '@tiptap/extension-text';
+import History from '@tiptap/extension-history';
 import { Placeholder } from '@tiptap/extensions/placeholder';
 import { useOnChangeHtml } from './useOnChangeHtml';
 import { useOnChangeText } from './useOnChangeText';
@@ -183,6 +184,7 @@ export const EnrichedTextInput = ({
       Document,
       Paragraph,
       Text,
+      History,
       EnrichedBold,
       EnrichedItalic,
       EnrichedUnderline,
