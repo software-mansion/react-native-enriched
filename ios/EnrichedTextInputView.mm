8354b17aece55d6b2f4d33c65136ce2e0e9520fc
Kacper Żółkiewski <74975508+kacperzolkiewski@users.noreply.github.com>
2026-06-02 12:28:33 +0200

    Apply suggestion from @kacperzolkiewski

--- ios/EnrichedTextInputView.mm
+++ ios/EnrichedTextInputView.mm
@@ -1593,7 +1593,7 @@ Class<RCTComponentViewProtocol> EnrichedTextInputViewCls(void) {
 
   // When text changed, anyTextMayHaveBeenModified runs tryUpdatingActiveStyles
   if ([_recentInputString isEqualToString:currentString]) {
-    // always update active styles
+    // update active styles
     [self tryUpdatingActiveStyles];
   }
 }
