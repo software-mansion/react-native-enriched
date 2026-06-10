31f37ffb377dc3c747ea5d5fd9b909d4ef1f297e
Kacper Żółkiewski <74975508+kacperzolkiewski@users.noreply.github.com>
2026-06-10 17:54:15 +0200

    feat(android): text alignment (#602)
    # Summary
    
    Adds text alignment for Android
    *(Note: `justify` alignment is not natively supported for single spans
    on Android and will not work).*
    
    ## Test Plan
    Run example app and play with `textAlignment` when different styles are
    toggled.
    Check if paragraphs/lists with different `textAlignment` than auto are
    properly parsed and contain `style="text-align:center|right"` inside
    HTML
    
    ## Screenshots / Videos
    
    
    https://github.com/user-attachments/assets/089543ad-93e4-4842-8b87-95316a3cb747
    
    
    ## Compatibility
    
    | OS      | Implemented |
    | ------- | :---------: |
    | iOS     |    ❌     |
    | Android |    ✅     |
    
    ## Checklist
    
    - [x] E2E tests are passing
    - [x] Required E2E tests have been added (if applicable)

--- android/src/main/java/com/swmansion/enriched/textinput/EnrichedTextInputView.kt
+++ android/src/main/java/com/swmansion/enriched/textinput/EnrichedTextInputView.kt
@@ -57,6 +57,7 @@ import com.swmansion.enriched.textinput.spans.EnrichedInputImageSpan
 import com.swmansion.enriched.textinput.spans.EnrichedLineHeightSpan
 import com.swmansion.enriched.textinput.spans.EnrichedSpans
 import com.swmansion.enriched.textinput.spans.interfaces.EnrichedInputSpan
+import com.swmansion.enriched.textinput.styles.AlignmentStyles
 import com.swmansion.enriched.textinput.styles.HtmlStyle
 import com.swmansion.enriched.textinput.styles.InlineStyles
 import com.swmansion.enriched.textinput.styles.ListStyles
@@ -85,6 +86,7 @@ class EnrichedTextInputView :
   val paragraphStyles: ParagraphStyles? = ParagraphStyles(this)
   val listStyles: ListStyles? = ListStyles(this)
   val parametrizedStyles: ParametrizedStyles? = ParametrizedStyles(this)
+  val alignmentStyles: AlignmentStyles? = AlignmentStyles(this)
   var isDuringTransaction: Boolean = false
   var isRemovingMany: Boolean = false
   var scrollEnabled: Boolean = true
@@ -154,6 +156,16 @@ class EnrichedTextInputView :
     defStyleAttr,
   ) {
     prepareComponent()
+  }
+
+  override fun scrollTo(
+    x: Int,
+    y: Int,
+  ) {
+    // Android's internal cursor tracker gets confused by ALIGN_CENTER + LeadingMarginSpan
+    // and attempts to scroll the text horizontally.
+    // We lock the horizontal scroll to 0 to prevent the view from shifting.
+    super.scrollTo(0, y)
   }
 
   override fun onCreateInputConnection(outAttrs: EditorInfo): InputConnection? {
@@ -976,6 +988,13 @@ class EnrichedTextInputView :
     if (!isValid) return
 
     parametrizedStyles?.setMentionSpan(text, indicator, attributes)
+  }
+
+  fun setTextAlignment(alignment: String) {
+    runAsATransaction {
+      alignmentStyles?.setAlignment(alignment)
+    }
+    selection?.validateStyles()
   }
 
   fun requestHTML(requestId: Int) {
